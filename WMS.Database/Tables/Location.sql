c54aa4c0be2fba4c1a517e0c7fefc9a23b9c674c
Alexis <alexisrk5@gmail.com>
2017-03-03 12:29:27 -0300

    Create the database #5
    .- Database was created

---
+++ WMS.Database/Tables/Location.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Location]
+(
+	[Name] NVARCHAR(255) NOT NULL PRIMARY KEY, 
+    [Description] NVARCHAR(255) NULL, 
+    [Type] INT NULL, 
+    [W] INT NULL, 
+    [B] INT NULL, 
+    [A] INT NULL, 
+    [X] INT NULL, 
+    [Y] INT NULL, 
+    [Z] INT NULL, 
+    [Capacity] INT NULL
+)
